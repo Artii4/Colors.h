da3d609c46ff1ce22179402c1c2dc5f31edbb575
Artii4 <noemailused@gmail.com>
2020-09-17 15:43:30 +0200

    First commit

---
+++ colors.h
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// This is a Color library for outputting text with colors.
+// You can use this like <iostream>, with the << operator
+
+namespace colors
+{
+    void setRed()
+    {
+    }
+}

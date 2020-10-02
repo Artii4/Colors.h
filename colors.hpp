c5024afdcbe2f772e2d5918dc529a420c8f6cdec
Artii4 <noemailused@gmail.com>
2020-10-02 15:05:57 +0200

    Initial commit of this branch

--- colors.hpp
+++ colors.hpp
@@ -1,372 +1,198 @@
 #include <iostream>
 #include <string>
 
-// This is a Color library for outputting text with colors
-// The library consists of functions that set colors, without the user having to enter escape codes manually
+/*
+ * COLORS.h VERSION 2 improvements:
+ *
+ * This library is in development, not recommended for developing purposes
+ * This branch is made because of multiple issues, but mainly because there
+ * are multiple functions for each color. This can be very hard to work with,
+ * especially as the library grows This version, version 2, is going to be in
+ * development as well as the previous branch.
+ *
+ * In this version, there is only one function to change a color instead of
+ * multiple function like "setRed()" and "setBlue()". This will make the
+ * library smaller.
+ *
+ * Project started on 2 oct 2020
+ * GPL 3 License
+ */
 
-// The layout is simple - To set a color, write "set", and then the color.
-// For example, "setBlack()" sets the color to be black
+namespace colors {
+enum class layer {
+    FOREGROUND,
+    BACKGROUND
+};
 
-// NOTE: Not all color or font changes are compatible with every single system. This works only on MAC/LINUX.
+enum class position {
+    TOEND,
+    TOBEGINNING,
+    ALL,
+    CLEAR
+};
 
-namespace colors
+// Enum with basic colors
+enum class color {
+    black, red, green,
+    yellow, blue, magenta, white, gray,
+    brightRed, brightGreen, brightYellow,
+    brightBlue, brightMagenta, brightCyan, brightWhite
+
+};
+
+void setColor(color color = color::red, layer layer = layer::FOREGROUND) {
+    if (layer == layer::FOREGROUND) {
+        switch(color) {
+        case color::black:
+            std::cout << "\033[30;40m";
+            break;
+        case color::red:
+            std::cout << "\033[31;41m";
+            break;
+        case color::green:
+            std::cout << "\033[32;42m";
+            break;
+    }
+}
+
+// RGB -> Change font color in RGB
+void rgb(int r = 0, int g = 0, int b = 0, layer foreground = layer::FOREGROUND)
 {
-    enum class layer
-    {
-        FOREGROUND,
-        BACKGROUND
-    };
+    std::string R = std::to_string(r);
+    std::string G = std::to_string(g);
+    std::string B = std::to_string(b);
 
-    enum class position
-    {
-        TOEND,
-        TOBEGINNING,
-        ALL,
-        CLEAR
-    };
-
-    // RGB -> Change font color in RGB
-    void rgb(int r = 0, int g = 0, int b = 0, layer foreground = layer::FOREGROUND)
-    {
-        std::string R = std::to_string(r);
-        std::string G = std::to_string(g);
-        std::string B = std::to_string(b);
-
-        std::string result = R + G + B + "m";
-        if (foreground == layer::FOREGROUND)
-        {
-            result = "\033[38;2" + result;
-        }
-        else
-        {
-            result = "\033[48;2" + result;
-        }
-        std::cout << result;
+    std::string result = R+G+B+"m";
+    if (foreground==layer::FOREGROUND) {
+        result = "\033[38;2"+result;
     }
-
-    void setBlack(layer foreground = layer::FOREGROUND)
-    {
-        if (foreground == layer::FOREGROUND)
-        {
-            std::cout << "\033[30m";
-        }
-        else
-        {
-            std::cout << "\033[40";
-        }
+    else {
+        result = "\033[48;2"+result;
     }
+    std::cout << result;
+}
 
-    void setRed(layer foreground = layer::FOREGROUND)
-    {
-        if (foreground == layer::FOREGROUND)
-        {
-            std::cout << "\033[31m";
-        }
-        else
-        {
-            std::cout << "\033[41m";
-        }
+void resetAll()
+{
+    std::cout << "\033[0m";
+}
+
+// Font-related functions
+
+void setBold()
+{
+    std::cout << "\033[1m";
+}
+
+void setThin()
+{
+    std::cout << "\033[2m";
+}
+
+void setItalic()
+{
+    std::cout << "\033[3m";
+}
+
+void setUnderline()
+{
+    std::cout << "\033[4m";
+}
+
+void setBlink()
+{
+    std::cout << "\033[5m";
+}
+
+// SetCursorTo -> Sets the cursor to a certain column and row
+
+void setCursorTo(int row = 0, int col = 0)
+{
+    std::string str = "\033["+std::to_string(row)+";"+std::to_string(col)+"H";
+    std::cout << str;
+}
+
+void moveCursorUp(int amount = 1)
+{
+    std::cout << "\033["+std::to_string(amount)+"A";
+}
+
+void moveCursorDown(int amount = 1)
+{
+    std::cout << "\033["+std::to_string(amount)+"B";
+}
+
+void moveCursorRight(int amount = 1)
+{
+    std::cout << "\033["+std::to_string(amount)+"C";
+}
+
+void moveCursorLeft(int amount = 1)
+{
+    std::cout << "\033["+std::to_string(amount)+"D";
+}
+
+// Clears screen
+
+void clear()
+{
+    system("clear");
+}
+
+// Resetting properties
+
+void resetColors(layer foreground = layer::FOREGROUND)
+{
+    if (foreground==layer::FOREGROUND) {
+        std::cout << "\033[39m";
     }
-
-    void setGreen(layer foreground = layer::FOREGROUND)
-    {
-        if (foreground == layer::FOREGROUND)
-        {
-            std::cout << "\033[32m";
-        }
-        else
-        {
-            std::cout << "\033[42m";
-        }
+    else {
+        std::cout << "\033[49m";
     }
+}
 
-    void setYellow(layer foreground = layer::FOREGROUND)
-    {
-        if (foreground == layer::FOREGROUND)
-        {
-            std::cout << "\033[33m";
-        }
-        else
-        {
-            std::cout << "\033[43m";
-        }
+void resetBoldness()
+{
+    std::cout << "\033[22m";
+}
+
+void noUnderline()
+{
+    std::cout << "\033[24m";
+}
+
+void noBlink()
+{
+    std::cout << "\033[25m";
+}
+
+// Line deletion
+void eraseLine(position pos = position::TOEND)
+{
+    if (pos==position::TOEND) {
+        std::cout << "\033[0K";
     }
-
-    void setBlue(layer foreground = layer::FOREGROUND)
-    {
-        if (foreground == layer::FOREGROUND)
-        {
-            std::cout << "\033[34m";
-        }
-        else
-        {
-            std::cout << "\033[44m";
-        }
+    else if (pos==position::TOBEGINNING) {
+        std::cout << "\033[1K";
     }
-
-    void setMagenta(layer foreground = layer::FOREGROUND)
-    {
-        if (foreground == layer::FOREGROUND)
-        {
-            std::cout << "\033[35m";
-        }
-        else
-        {
-            std::cout << "\033[45m";
-        }
+    else {
+        std::cout << "\033[2K";
     }
+}
 
-    void setCyan(layer foreground = layer::FOREGROUND)
-    {
-        if (foreground == layer::FOREGROUND)
-        {
-            std::cout << "\033[36m";
-        }
-        else
-        {
-            std::cout << "\033[46m";
-        }
+void eraseScreen(position pos = position::TOEND)
+{
+    if (pos==position::TOEND) {
+        std::cout << "\033[J";
     }
-
-    void setWhite(layer foreground = layer::FOREGROUND)
-    {
-        if (foreground == layer::FOREGROUND)
-        {
-            std::cout << "\033[37m";
-        }
-        else
-        {
-            std::cout << "\033[47m";
-        }
+    else if (pos==position::TOBEGINNING) {
+        std::cout << "\033[1J";
     }
-
-    // Brighter colors
-    void setGray(layer foreground = layer::FOREGROUND)
-    {
-        if (foreground == layer::FOREGROUND)
-        {
-            std::cout << "\033[90m";
-        }
-        else
-        {
-            std::cout << "\033[100m";
-        }
+    else if (pos==position::ALL) {
+        std::cout << "\033[2J";
     }
-
-    void setBrightRed(layer foreground = layer::FOREGROUND)
-    {
-        if (foreground == layer::FOREGROUND)
-        {
-            std::cout << "\033[91m";
-        }
-        else
-        {
-            std::cout << "\033[101m";
-        }
+    else if (pos==position::CLEAR) {
+        std::cout << "\033[3J";
     }
-
-    void setBrightGreen(layer foreground = layer::FOREGROUND)
-    {
-        if (foreground == layer::FOREGROUND)
-        {
-            std::cout << "\033[92m";
-        }
-        else
-        {
-            std::cout << "\033[102m";
-        }
-    }
-
-    void setBrightYellow(layer foreground = layer::FOREGROUND)
-    {
-        if (foreground == layer::FOREGROUND)
-        {
-            std::cout << "\033[93m";
-        }
-        else
-        {
-            std::cout << "\033[103m";
-        }
-    }
-
-    void setBrightBlue(layer foreground = layer::FOREGROUND)
-    {
-        if (foreground == layer::FOREGROUND)
-        {
-            std::cout << "\033[94m";
-        }
-        else
-        {
-            std::cout << "\033[104m";
-        }
-    }
-
-    void setBrightMagenta(layer foreground = layer::FOREGROUND)
-    {
-        if (foreground == layer::FOREGROUND)
-        {
-            std::cout << "\033[95m";
-        }
-        else
-        {
-            std::cout << "\033[105m";
-        }
-    }
-
-    void setBrightCyan(layer foreground = layer::FOREGROUND)
-    {
-        if (foreground == layer::FOREGROUND)
-        {
-            std::cout << "\033[96m";
-        }
-        else
-        {
-            std::cout << "\033[106m";
-        }
-    }
-
-    void setBrightWhite(layer foreground = layer::FOREGROUND)
-    {
-        if (foreground == layer::FOREGROUND)
-        {
-            std::cout << "\033[97m";
-        }
-        else
-        {
-            std::cout << "\033[107m";
-        }
-    }
-
-    void resetAll()
-    {
-        std::cout << "\033[0m";
-    }
-
-    // Font-related functions
-
-    void setBold()
-    {
-        std::cout << "\033[1m";
-    }
-
-    void setThin()
-    {
-        std::cout << "\033[2m";
-    }
-
-    void setItalic()
-    {
-        std::cout << "\033[3m";
-    }
-
-    void setUnderline()
-    {
-        std::cout << "\033[4m";
-    }
-
-    void setBlink()
-    {
-        std::cout << "\033[5m";
-    }
-
-    // SetCursorTo -> Sets the cursor to a certain column and row
-
-    void setCursorTo(int row = 0, int col = 0)
-    {
-        std::string str = "\033[" + std::to_string(row) + ";" + std::to_string(col) + "H";
-        std::cout << str;
-    }
-
-    void moveCursorUp(int amount = 1)
-    {
-        std::cout << "\033[" + std::to_string(amount) + "A";
-    }
-
-    void moveCursorDown(int amount = 1)
-    {
-        std::cout << "\033[" + std::to_string(amount) + "B";
-    }
-
-    void moveCursorRight(int amount = 1)
-    {
-        std::cout << "\033[" + std::to_string(amount) + "C";
-    }
-
-    void moveCursorLeft(int amount = 1)
-    {
-        std::cout << "\033[" + std::to_string(amount) + "D";
-    }
-
-    // Clears screen
-
-    void clear()
-    {
-        system("clear");
-    }
-
-    // Resetting properties
-
-    void resetColors(layer foreground = layer::FOREGROUND)
-    {
-        if (foreground == layer::FOREGROUND)
-        {
-            std::cout << "\033[39m";
-        }
-        else
-        {
-            std::cout << "\033[49m";
-        }
-    }
-
-    void resetBoldness()
-    {
-        std::cout << "\033[22m";
-    }
-
-    void noUnderline()
-    {
-        std::cout << "\033[24m";
-    }
-
-    void noBlink()
-    {
-        std::cout << "\033[25m";
-    }
-
-    // Line deletion
-    void eraseLine(position pos = position::TOEND)
-    {
-        if (pos == position::TOEND)
-        {
-            std::cout << "\033[0K";
-        }
-        else if (pos == position::TOBEGINNING)
-        {
-            std::cout << "\033[1K";
-        }
-        else
-        {
-            std::cout << "\033[2K";
-        }
-    }
-
-    void eraseScreen(position pos = position::TOEND)
-    {
-        if (pos == position::TOEND)
-        {
-            std::cout << "\033[J";
-        }
-        else if (pos == position::TOBEGINNING)
-        {
-            std::cout << "\033[1J";
-        }
-        else if (pos == position::ALL)
-        {
-            std::cout << "\033[2J";
-        }
-        else if (pos == position::CLEAR)
-        {
-            std::cout << "\033[3J";
-        }
-    }
+}
 }; // namespace colors
